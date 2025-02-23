59c18ff5bcf82489c25c32bbf8afd68279d1f201
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-23 22:26:09 +0100

    List is created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
 
+public class Main {
+    public static <T> void main(String[] args) {
+
+        List<String> list = List.of("Jack", "John", "Sam", "Jack");
+        List<Person> people = List.of(
+                new Person("Jack", 10),
+                new Person("Sam", 21),
+                new Person("Margo", 14),
+                new Person("Slava", 19),
+                new Person("Jack", 10));
     }
 }
