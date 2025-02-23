aea1ded40c7cfd94f5f718d0f2c5d9ef492bc875
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-23 22:58:35 +0100

    Class Main edited and finished

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
-    public static <T> void main(String[] args) {
-
-        List<String> list = List.of("Jack", "John", "Sam", "Jack");
-        List<Person> people = List.of(
-                new Person("Jack", 10),
-                new Person("Sam", 21),
-                new Person("Margo", 14),
-                new Person("Slava", 19),
-                new Person("Jack", 10));
+        public static void main(String[] args) {
+            List<String> stringList = List.of("Jack", "John", "Sam", "Jack");
+            List<Person> peopleList = List.of(
+                    new Person("Jack", 10),
+                    new Person("Sam", 21),
+                    new Person("Margo", 14),
+                    new Person("Slava", 19),
+                    new Person("Jack", 10)
+            );
+            printList(stringList);
+            printList(peopleList);
+        }
+        private static <T> void printList(List<T> list) {
+            System.out.println("List: " + list);
+            if (hasDuplicate(list)) {
+                System.out.println("The list contains duplicate elements.");
+            } else {
+                System.out.println("The list does not contain duplicate elements.");
+            }
+            System.out.println();
+        }
+        private static <T> boolean hasDuplicate(List<T> list) {
+            Set<T> set = new HashSet<>();
+            for (T item : list)
+                if (!set.add(item)) {
+                    return true;
+                }
+            return false;
+        }
     }
-}
+
+
+
