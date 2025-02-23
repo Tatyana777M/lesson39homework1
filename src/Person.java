807e6500cff5d2864a41fddf2e46d2f8c0fc05fd
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-23 22:57:29 +0100

    Class Person has finished

--- src/Person.java
+++ src/Person.java
@@ -1,40 +1,40 @@
 
 public class Person {
 
-        private String name;
-        private int age;
+    private String name;
+    private int age;
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
     public String getName() {
         return name;
     }
-
     public int getAge() {
         return age;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Person person = (Person) obj;
+        return age == person.age;
+//                    && Object.equals (name, person.name);
 
-    public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
-
-        }
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass()!= obj.getClass())
-                return false;
-            Person person = (Person) obj;
-            return age == person.age &&
-                    Object.equals (name, person.name);
-
-        }
-        @Override
-        public int hashCode() {
-            return Object.hash(name, age);
-        }
-        @Override
-        public String toString() {
-            return name + " (" + age + ")";
-        }
     }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+}
 
